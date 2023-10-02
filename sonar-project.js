0df2320a91684db7001c125f2067c9e7993e2baf
Cloud User <ec2-user@ip-172-31-89-250.ec2.internal>
2023-10-02 03:25:15 +0000

    create a new file named sonar and add a content to it

---
+++ sonar-project.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://34.207.61.23:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsAcadalearning',
+	    'sonar.login': '749a047ee12c12a8bd35ab19755d723a5394fb9e',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
